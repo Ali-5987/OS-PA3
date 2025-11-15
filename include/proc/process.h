155fdf2cd060a89e5e9a01eb5e118ba4a6773506
Ali21 <27100083@lums.edu.pk>
2025-11-16 00:59:18 +0500

    completed process create func

---
+++ include/proc/process.h
@@ -0,0 +1,50 @@
+#ifndef PROCESS_H
+#define PROCESS_H
+ 
+ #include <fs/vfs.h>
+#include <mm/vmm.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include "interrupts.h"
+
+#define STATE_TERMINATED -1
+ typedef struct process process_t;
+typedef struct thread thread_t;
+
+ typedef struct process{
+    uint32_t Pid;
+    char name[32];
+    thread_t* threads;
+    uint32_t priority;
+    pagedir_t* dir;
+} process_t;
+ 
+typedef struct thread
+{
+    uint32_t tid;
+    uint32_t state;
+    process_t* parent;
+    uint32_t* kernel;
+    interrupt_context_t* trap_frame;
+    struct thread_t* next;
+} thread_t;
+
+ 
+ void process_create(process_t* process, const char* name, int32_t priority);
+ void process_destroy(process_t* process);
+ int32_t process_spawn(const char* filename);
+ int32_t process_fork();
+ process_t* process_find_by_pid(uint32_t pid);
+ void process_exit(process_t* process, int32_t status);
+thread_t* _get_main_thread(process_t* process);
+thread_t* thread_create(process_t* parent_process, void* entry, void* arg);
+int32_t thread_destroy(thread_t* thread);
+void scheduler_init(void);
+void scheduler_tick(interrupt_context_t* context);
+void scheduler_switch(thread_t* next_thread);
+void scheduler_post(thread_t* thread);
+process_t* get_current_proc(void);
+thread_t* get_current_thread(void);
+
+#endif // PROCESS_H
--- proc/process.c
+++ proc/process.c
@@ -2,11 +2,34 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <stdbool.h>
-
+#include <../include/proc/process.h>
 #define LOG_MOD_NAME 	"PRC"
 #define LOG_MOD_ENABLE  1
 #include <log.h>
-
-int32_t process_fork () {
-
+static int current_pid=1;
+void process_create(process_t* process, const char* name, int32_t priority)
+{   process->Pid = current_pid;
+    current_pid++;
+    process->dir = vmm_create_address_space();
+    memcpy(process->dir,vmm_get_kerneldir(),sizeof(pagedir_t));
+    process->priority = priority;
+    strncpy(process->name, name,31);
+    process->threads = NULL;
 }
+ void process_destroy(process_t* process)
+ {
+    
+ }
+ int32_t process_spawn(const char* filename){}
+ int32_t process_fork(){}
+ process_t* process_find_by_pid(uint32_t pid){}
+ void process_exit(process_t* process, int32_t status){}
+thread_t* _get_main_thread(process_t* process){}
+thread_t* thread_create(process_t* parent_process, void* entry, void* arg){}
+int32_t thread_destroy(thread_t* thread){}
+void scheduler_init(void){}
+void scheduler_tick(interrupt_context_t* context){}
+void scheduler_switch(thread_t* next_thread){}
+void scheduler_post(thread_t* thread){}
+process_t* get_current_proc(void){}
+thread_t* get_current_thread(void){}
