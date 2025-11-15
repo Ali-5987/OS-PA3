47850ed2325ddb1ca3cc66fb1e020d82a7b6e54b
Ali21 <27100083@lums.edu.pk>
2025-11-15 19:39:02 +0500

    debugged elf

--- proc/elf.c
+++ proc/elf.c
@@ -4,7 +4,7 @@
 #include <../include/fs/vfs.h>
 bool elf_check_hdr(elf_header_t* hdr)
 {
-    if (hdr->e_ident[0] != "0x7F" ||hdr->e_ident[1] != "E" || hdr->e_ident[2] != "L" || hdr->e_ident[2] != "F")
+    if (hdr->e_ident[0] != 0x7F ||hdr->e_ident[1] != 'E' || hdr->e_ident[2] != 'L' || hdr->e_ident[3] != 'F')
    return false;
    if (hdr->e_type != ELF_TYPE_EXEC)
    return false;
@@ -21,12 +21,13 @@ int32_t elf_load_seg(file_t* file, pagedir_t* dir, elf_phdr_t* phdr)
     return -1;
     uint32_t start_addr = PAGE_ALIGN_UP(phdr->p_vaddr);
     uint32_t end_addr = PAGE_ALIGN_DWN(phdr->p_vaddr + phdr->p_memsz);
+    uint32_t size = end_addr - start_addr;
     uint32_t num_pages = (end_addr - start_addr) / VMM_PAGE_SIZE;
     uint32_t allgined_vaddr = PAGE_ALIGN_UP(phdr->p_vaddr);
-   // uint32_t size = phdr->p_memsz + (allgined_vaddr - phdr->p_vaddr);
-    vmm_alloc_region(dir,allgined_vaddr,num_pages, PDE_USER | PDE_PRESENT | PDE_WRITABLE);
+    if (!vmm_alloc_region(dir,allgined_vaddr,size, PDE_USER | PDE_PRESENT | PDE_WRITABLE))
+    return -1;
+    file->f_offset = phdr->p_offset;
     vfs_read(file, (void*)phdr->p_vaddr, phdr->p_filesz);
-
     if (phdr->p_memsz > phdr->p_filesz)
     {
         memset((void*) (phdr->p_vaddr + phdr->p_filesz), 0,phdr->p_memsz - phdr->p_filesz);
